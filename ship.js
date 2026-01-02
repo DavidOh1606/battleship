1a72fc44469881145cc6520716040550730e6919
DavidOh1606 <david.oh1606@gmail.com>
2026-01-02 14:26:24 -0800

    Create testing for hit and isSunk functions and create hit and isSunk functions

--- ship.js
+++ ship.js
@@ -2,6 +2,23 @@ function Ship(length) {
     this.length = length;
     this.timesHit = 0;
     this.sunk = false;
+
+    this.hit = function() {
+        this.timesHit++;
+
+        this.sunk = this.isSunk();
+
+    }
+
+    this.isSunk = function() {
+        if (this.timesHit >= this.length) {
+            return true;
+        }
+
+        return false;
+    }
+
+
 }
 
 
